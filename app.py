ec97c673dc9631799fdb1f83d5141e355cf115fa
Aniket Sonavane <34575457+aniketsnv-1997@users.noreply.github.com>
2021-10-05 11:57:19 +0530

    linked index.html to the application

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/home', methods=['GET', 'POST'])
 def DisplayProductOrderForm():
-    return render_template('muse.html')
+    return render_template('index.html')
 
 
 # @app.route('/bot', methods=['POST'])
