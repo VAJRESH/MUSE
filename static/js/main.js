32405161d3805e431dbdf7c48e06b0b85ce9d1e1
Aniket Sonavane <34575457+aniketsnv-1997@users.noreply.github.com>
2021-10-08 13:30:54 +0530

    link

--- static/js/main.js
+++ static/js/main.js
@@ -434,7 +434,7 @@ function displayCart() {
   } else {
     const htmlStr = `
     <td colspan='4' class='text-center btn-link h5 p-4 bg-light'>
-      <a href="{{ url_for("DisplayProductOrderForm") }}">
+      <a href="https://muse-by-vivekanand-seva-mandal.herokuapp.com/">
       <span class='text-danger'>
         Go to the products page to add items to the cart
       </span>
