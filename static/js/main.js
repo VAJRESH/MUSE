5984c0e5ce560ab895acd38324fc059c54a69d7c
Aniket Sonavane <34575457+aniketsnv-1997@users.noreply.github.com>
2021-10-08 13:21:33 +0530

    update to links

--- static/js/main.js
+++ static/js/main.js
@@ -434,9 +434,9 @@ function displayCart() {
   } else {
     const htmlStr = `
     <td colspan='4' class='text-center btn-link h5 p-4 bg-light'>
-      <a href='index.html'>
+      <a href=href="{{ url_for("DisplayProductOrderForm") }}">
       <span class='text-danger'>
-        Please Add Items to Cart
+        Go to the products page to add items to the cart
       </span>
       </a>
     </td>
